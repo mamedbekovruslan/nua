08a13635c700142634b8f4cf707d44e842a99dfa
mamedbekovRA <mamedbekovruslan@yandex.ru>
2025-04-11 18:06:00 +0300

    Поправить текст в кратком описании

--- src/pages/Home/components/Description.tsx
+++ src/pages/Home/components/Description.tsx
@@ -8,7 +8,13 @@ export const Description = () => {
         <img className={styles.descriptionImg} src={aboutProjectImg} />
       </div>
       <div className={styles.descriptionTextContainer}>
-        <span>Наш проект нацелен на ваши достижения</span>
+        <span className={styles.descriptionTextTitle}>
+          Наш проект нацелен на ваши достижения
+        </span>
+        <span className={styles.descriptionTextAdd}>
+          Вы можете начать свой путь с помощью гайдов. Или же найти что-то новое
+          для себя даже если вы опытный атлет.
+        </span>
       </div>
     </div>
   );
