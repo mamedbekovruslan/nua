f198072805293e3b477a439682a6ab443c46ce76
mamedbekovRA <mamedbekovruslan@yandex.ru>
2025-04-11 16:53:51 +0300

    Добавить стили на scrollbar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,30 +3,33 @@ import styles from "./Home.module.css";
 
 export const Home = () => {
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.bannerInfo}>
-        <div className={styles.descriptionContainer}>
-          <div className={styles.title}>Сделай себя сам</div>
-          <div className={styles.description}>
-            Наш проект нацелен на ваши достижения
+    <>
+      <div className={styles.mainContainer}>
+        <div className={styles.bannerInfo}>
+          <div className={styles.descriptionContainer}>
+            <div className={styles.title}>Сделай себя сам</div>
+            <div className={styles.description}>
+              Наш проект нацелен на ваши достижения
+            </div>
+            <div className={styles.buttonContainer}>
+              <button className={styles.button} id={styles.newbie}>
+                Я новичок
+              </button>
+              <button className={styles.button} id={styles.skilled}>
+                Я опытный
+              </button>
+            </div>
           </div>
-          <div className={styles.buttonContainer}>
-            <button className={styles.button} id={styles.newbie}>
-              Я новичок
-            </button>
-            <button className={styles.button} id={styles.skilled}>
-              Я опытный
-            </button>
+          <div className={styles.container}>
+            <div className={styles.chevron}></div>
+            <div className={styles.chevron}></div>
+            <div className={styles.chevron}></div>
+            <span className={styles.text}>Подробнее ниже</span>
           </div>
         </div>
-        <div className={styles.container}>
-          <div className={styles.chevron}></div>
-          <div className={styles.chevron}></div>
-          <div className={styles.chevron}></div>
-          <span className={styles.text}>Подробнее ниже</span>
-        </div>
+        <img className={styles.img} src={backgroundImg} alt="Background" />
       </div>
-      <img className={styles.img} src={backgroundImg} alt="Background" />
-    </div>
+      <div>ХА</div>
+    </>
   );
 };
