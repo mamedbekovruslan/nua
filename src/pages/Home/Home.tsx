af369b5e9fc645547bd7d4274043fa5a44d076f0
mamedbekovRA <mamedbekovruslan@yandex.ru>
2025-04-11 12:54:53 +0300

    Поправить оступы

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,17 +4,25 @@ import styles from "./Home.module.css";
 export const Home = () => (
   <div className={styles.mainContainer}>
     <div className={styles.bannerInfo}>
-      <div className={styles.title}>Сделай себя сам</div>
-      <div className={styles.description}>
-        Наш проект нацелен на ваши достижения
+      <div>
+        <div className={styles.title}>Сделай себя сам</div>
+        <div className={styles.description}>
+          Наш проект нацелен на ваши достижения
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button} id={styles.newbie}>
+            Я новичок
+          </button>
+          <button className={styles.button} id={styles.skilled}>
+            Я опытный
+          </button>
+        </div>
       </div>
-      <div className={styles.buttonContainer}>
-        <button className={styles.button} id={styles.newbie}>
-          Я новичок
-        </button>
-        <button className={styles.button} id={styles.skilled}>
-          Я опытный
-        </button>
+      <div className={styles.container}>
+        <div className={styles.chevron}></div>
+        <div className={styles.chevron}></div>
+        <div className={styles.chevron}></div>
+        <span className={styles.text}>Scroll down</span>
       </div>
     </div>
     <img className={styles.img} src={backgroundImg} />
